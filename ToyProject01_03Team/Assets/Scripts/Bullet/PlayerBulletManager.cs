0ae8ac72132140cb1d0c9dc3aba5a9120eac0a95
Jade <jade16work@gmail.com>
2026-02-15 17:12:00 +0900

    [refactor] Player bullet pool
    Dict -> stack

--- ToyProject01_03Team/Assets/Scripts/Bullet/PlayerBulletManager.cs
+++ ToyProject01_03Team/Assets/Scripts/Bullet/PlayerBulletManager.cs
@@ -2,16 +2,55 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBulletManager : MonoBehaviour
+public class PlayerBulletManager : SceneSingleton<PlayerBulletManager>
 {
     [SerializeField] GameObject _bulletPrefab;
-    Dictionary<GameObject,Queue<GameObject>> bullets = new Dictionary<GameObject,Queue<GameObject>>();
-    
-    public static PlayerBulletManager Instance;
 
+    [SerializeField] private int _defaultSize;
+    
+    // Dictionary<GameObject,Queue<GameObject>> bullets = new Dictionary<GameObject,Queue<GameObject>>();
+    private Stack<PlayerBullet> _pool;
+    
+    
     void Awake()
     {
-        Instance = this;
+        Init();
+        SceneSingletonInit();
+    }
+
+    private void Init()
+    {
+        _pool= new Stack<PlayerBullet>(_defaultSize);
+    }
+
+    private void ShootBullet(Transform muzzle)
+    {
+        PlayerBullet bullet;
+        
+        if (_pool.Count == 0)   bullet = CreateBullet();
+        else                    bullet = _pool.Pop();
+        
+        bullet.gameObject.SetActive(true);
+        bullet.transform.position = muzzle.position;
+        bullet.transform.rotation = muzzle.rotation;
+        
+        IShootable shootable = bullet as IShootable;
+        shootable.OnSpawn();
+    }
+
+    private void DespawnBullet(PlayerBullet bullet)
+    {
+        _pool.Push(bullet);
+        bullet.gameObject.SetActive(false);
+        
+        IShootable shootable = bullet as IShootable;
+        shootable.OnDespawn();
+    }
+
+    private PlayerBullet CreateBullet()
+    {
+        // Bullet 생산
+        // _pool.Push()
     }
 
     public GameObject ShootBullet(GameObject bulletPrefab, Vector3 bulletPos, Quaternion bulletRot)
