24c9a70e3c6806320a0c2e50103a3085baa31c25
Jade <jade16work@gmail.com>
2026-02-18 11:09:51 +0900

    [feat] GameOver popup

---
+++ ToyProject01_03Team/Assets/Scripts/UI/GameOverUI.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverUI : MonoBehaviour
+{
+    [SerializeField] private Button _toTitleButton;
+
+    private void Awake()        => Init();
+    private void OnEnable()     => _toTitleButton.onClick.AddListener(ToTitle);
+    private void Start()        => gameObject.SetActive(false);
+    private void OnDisable()    => _toTitleButton.onClick.RemoveListener(ToTitle);
+    private void OnDestroy()    => Dispose();
+
+    private void Init()
+    {
+        GameManager.Instance.OnGameOver += Activate;
+    }
+
+    private void Dispose()
+    {
+        GameManager.Instance.OnGameOver -= Activate;
+    }
+
+    private void Activate()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void ToTitle()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
