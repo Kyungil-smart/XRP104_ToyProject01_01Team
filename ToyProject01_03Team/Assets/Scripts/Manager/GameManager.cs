6d9f712550c9826516957871095fff9469534c60
Jade <jade16work@gmail.com>
2026-02-18 10:05:10 +0900

    [feat] GameManager

--- ToyProject01_03Team/Assets/Scripts/Manager/GameManager.cs
+++ ToyProject01_03Team/Assets/Scripts/Manager/GameManager.cs
@@ -1,16 +1,49 @@
+using System;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    public int CurrentStage;
+    [field: SerializeField] public int CurrentStage { get; set; }
+    public event Action OnGameStart;
+    public event Action<bool> OnGamePause;
+    public event Action OnStageClear;
+    public event Action OnGameOver;
+
+    public bool IsGameRunning { get; private set; }
+    public bool IsGamePaused { get; private set; }
     
+
     private void Awake()
     {
         SingletonInit();
+        
+        IsGameRunning = false;
+        IsGamePaused = false;
+    }
+
+    public void GameStart()
+    {
+        IsGameRunning = true;
+        OnGameStart?.Invoke();
+    }
+
+    public void GamePause(bool isPaused)
+    {
+        if (!IsGameRunning) return;
+        
+        IsGamePaused = isPaused;
+        OnGamePause?.Invoke(isPaused);
+    }
+
+    public void GameOver()
+    {
+        IsGameRunning = false;
+        OnGameOver?.Invoke();
     }
 
     public void StageClear()
     {
-        Debug.Log("StageClear");
+        OnStageClear?.Invoke();
+        GameOver();
     }
 }
